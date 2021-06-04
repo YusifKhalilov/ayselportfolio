1ddf7171dd4060e614a853c28662b4c6f74f3f59
YusifKhalilov <joseph.khalilov@gmail.com>
2021-06-04 11:04:02 -0700

    Removed Context connection

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 /* --------------------------------- IMPORTS -------------------------------- */
-// context
-import { AppMainProvider } from './AppContext';
 // components
 import Menu from './components/common/Menu/Menu';
 import HomePage from './components/modules/HomePage/HomePage';
@@ -8,9 +6,9 @@ import HomePage from './components/modules/HomePage/HomePage';
 /* ----------------------------- HOME COMPONENT ----------------------------- */
 export default function Home() {
 	return (
-		<AppMainProvider>
+		<>
 			<Menu />
 			<HomePage />
-		</AppMainProvider>
+		</>
 	);
 }
